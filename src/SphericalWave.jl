895d2ed820a307af4ca8a53a69b5cbf3d1f49ace
Peter Simon <psimon0420@gmail.com>
2024-06-23 11:03:57 -0700

    Add doc string and new method for sph2cut

--- src/SphericalWave.jl
+++ src/SphericalWave.jl
@@ -793,6 +793,40 @@ function _filter_qmodes_by_power(qsmns, pwrtol)
 end
 
 
+"""
+    sph2cut(swefile; theta, phi, ipol) -> cut::TicraCut
+
+    sph2cut(swe; theta, phi, ipol) -> cut::TicraCut
+
+Convert a set of Q-type spherical wave modal coefficients to far-field electric field 
+values, returned as a `TicraCut` object. 
+
+The single positional input argument can be either a string containing the name 
+of a Ticra-compatible Q-type spherical wave file, or the returned value from reading 
+such a file with `read_sphfile`.
+
+## Optional Keyword Arguments:
+- `theta`: An abstract range denoting the desired polar angles (colattitudes) 
+  in degrees at which the field should be evaluated. If an empty range is provided (the default), then
+  the values will be determined automatically by examining the modal content in `swe`.
+- `phi`: An abstract range denoting the desired azimuthal angles in degrees at 
+  which the field should be evaluated.  If an empty range is provided (the default), then
+  the values will be determined automatically by examining the modal content in `swe`.
+- `ipol`:  An integer in the range 0:3 denoting the desired polarization decomposition 
+  for the computed field values. The meanings of these values are:
+    - 0: (Default value) Choose the basis among choices 1, 2, or 3 that produces the largest peak copol magnitude.
+    - 1: Use a (θ̂, ϕ̂) basis.
+    - 2: Use a (R̂, L̂) (i.e., circular polarization) basis.
+    - 3: Use a (ĥ, v̂) (Ludwig 3) basis.
+
+## Usage Example
+    cut = sph2cut("testfile.sph"; phi=0:5:355, theta=0:1:180, ipol=2)
+"""
+function sph2cut(sphfile::AbstractString; kwargs...)
+    swe = read_sphfile(sphfile)
+    sph2cut(swe; kwargs...)
+end
+
 function sph2cut(swe::SWEQPartition; 
     theta::AbstractRange=0.0:-1.0:1.0, 
     phi::AbstractRange=0.0:-1.0:1.0,
