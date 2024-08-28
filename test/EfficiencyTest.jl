4e39512f2e1ee62787c2e2cef85f5d061aadb6a5
Peter Simon <psimon0420@gmail.com>
2024-08-28 16:04:04 -0700

    Update tests

--- test/EfficiencyTest.jl
+++ test/EfficiencyTest.jl
@@ -5,13 +5,15 @@ using SafeTestsets
     cd(@__DIR__)
     cutfile = joinpath(@__DIR__, "center_element_rhcp_excited.cut")
     t1 = sor_efficiency(cutfile, F=10, D=18, Oc=0.4, pol=:l3h, dz=-0.1)
-    @test t1.ηₛ ≈ 0.8727422758602933
+    @test t1.ηₗₒₛₛ ≈ 0.9733666461505174
+    @test t1.ηₛₚ ≈ 0.8727422758602933
+    @test t1.ηᵢₗ ≈ 0.8745616072685025
+    @test t1.ηₚₕ ≈ 0.9642498710590889
+    @test t1.ηₚₒₗ ≈ 1.0
     @test t1.ηₓ ≈ 0.4985615310506499
-    @test t1.ηᵢₗ ≈ 0.8519300087313848
-    @test t1.ηₚ ≈ 0.9635018253504125
 
     # Higher resolution for the ϕ integrals
-    cut = read_cutfile(cutfile)
+    cut = read_cut(cutfile)
     nphi = 2 * length(get_phi(cut))
     Δphi = 360 / nphi
     phi = range(0,360-Δphi, nphi)
