f58a9d7787fbd0becf693ba12ed021c18a737ca3
Peter Simon <psimon0420@gmail.com>
2025-01-23 15:21:11 -0800

    Fix typo in symsqueeze docstring

--- src/Cut.jl
+++ src/Cut.jl
@@ -1138,7 +1138,7 @@ end
 """
     symsqueeze(cut::Cut) -> cut2::Cut
 
-Squeeze a symmetric cut with ϕ values equally distributed in [0,360) into [0, 180].
+Squeeze a symmetric cut with ϕ values equally distributed in [0,360) into [0, 180).
 
 The ϕ values must define nonredundant cuts.  I.e., `cut.phi` must be equivalent to the vector
 `[0, Δϕ, 2Δϕ, 3Δϕ, ..., (360 - Δϕ)]`, with `length(cut.phi)` being an odd number, so that 180
