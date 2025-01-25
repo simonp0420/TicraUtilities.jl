17be49f9eab96b1f632cde00440f6202c1249a9b
Peter Simon <psimon0420@gmail.com>
2025-01-24 16:16:44 -0800

    Update docstring for cut2sph

--- src/SphericalWave.jl
+++ src/SphericalWave.jl
@@ -600,7 +600,8 @@ The single positional input argument can be either a string containing the name
 of a Ticra-compatible, spherical polar cut file, or the returned value of type `Cut` 
 (or a vector of `Cut` objects) that results from reading such a file with `read_cutfile`.  
 The output of this function can be passed to `write_sphfile` to create a Ticra-compatible 
-file of Q-type spherical wave coefficients. 
+file of Q-type spherical wave coefficients. If the input cut contains 3 polarization slots,
+the third slot will be discarded before performing the transformation.
 
 If the input cuts extend in θ only to θ₀ < 180°, then it will be assumed that
 the fields are identically zero for θ₀ < θ ≤ 180°.
